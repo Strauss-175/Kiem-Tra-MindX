b6187945e4e1ed2585ab97071f758e9712bbd87e
Strauss-175 <144643280+Strauss-175@users.noreply.github.com>
2023-10-18 22:17:00 +0700

    Add files via upload

---
+++ Kiem-Tra-MindX/index.js
@@ -0,0 +1,22 @@
+const A = parseInt(document.getElementById("A"));
+const B = parseInt(document.getElementById("B"));
+const button = document.getElementById("button");
+let N = [];
+let a = [];
+let b = [];
+function check() {
+  for (let i1 = A; i1 <= B; i1++) {
+    N.push(i1);
+  }
+  for (let i2 = 2; i2 < A - 1; i2++) {
+    if (A % i2 !== 0) {
+      a.push(i2);
+    }
+  }
+  for (let i3 = 2; i3 < B - 1; i3++) {
+    if (B % i3 !== 0) {
+      B.push(i3);
+    }
+  }
+}
+button.onclick = check;
