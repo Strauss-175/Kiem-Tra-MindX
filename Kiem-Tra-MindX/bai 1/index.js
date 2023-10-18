5ed937612cb0b5785685ac89aca5cad1d8f0a031
Strauss-175 <144643280+Strauss-175@users.noreply.github.com>
2023-10-18 22:14:43 +0700

    kiem tra mindx

--- Kiem-Tra-MindX/bai 1/index.js
+++ Kiem-Tra-MindX/bai 1/index.js
@@ -1,19 +1,22 @@
 const A = parseInt(document.getElementById("A"));
 const B = parseInt(document.getElementById("B"));
 const button = document.getElementById("button");
+let N = [];
 let a = [];
 let b = [];
 function check() {
-  for (let i1 = 2; i1 < A - 1; i1++) {
-    if (A % i1 !== 0) {
-      a.push(i);
+  for (let i1 = A; i1 <= B; i1++) {
+    N.push(i1);
+  }
+  for (let i2 = A; i2 < A - 1; i2++) {
+    if (A % i2 !== 0) {
+      a.push(i2);
     }
   }
-  for (let i2 = 0; i2 < B -1 ; i2++) {
-    if (B % i2 !== 0){
-        B.push(i2);
+  for (let i3 = 0; i3 < B - 1; i3++) {
+    if (B % i3 !== 0) {
+      B.push(i3);
     }
-}
+  }
 }
 button.onclick = check;
-
