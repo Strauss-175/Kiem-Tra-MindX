fe4de21932ce307eb8277e6f8a4c2f4e53cc03f0
Strauss-175 <144643280+Strauss-175@users.noreply.github.com>
2023-10-18 22:18:38 +0700

    Add files via upload

--- Kiem-Tra-MindX/bai 1/index.js
+++ Kiem-Tra-MindX/bai 1/index.js
@@ -8,12 +8,12 @@ function check() {
   for (let i1 = A; i1 <= B; i1++) {
     N.push(i1);
   }
-  for (let i2 = A; i2 < A - 1; i2++) {
+  for (let i2 = 2; i2 < A - 1; i2++) {
     if (A % i2 !== 0) {
       a.push(i2);
     }
   }
-  for (let i3 = 0; i3 < B - 1; i3++) {
+  for (let i3 = 2; i3 < B - 1; i3++) {
     if (B % i3 !== 0) {
       B.push(i3);
     }
